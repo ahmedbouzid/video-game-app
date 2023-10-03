dfb1bedc797c5d65a9714c52eedd2159364a7573
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-10-03 15:19:56 +0200

    prepare general styling and import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
+import { GaugeModule } from 'angular-gauge';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule ,
+    FormsModule ,
+    HttpClientModule ,
+    GaugeModule.forRoot() ,
+    MatIconModule ,
+    MatFormFieldModule ,
+    MatTabsModule ,
+    MatSelectModule ,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
