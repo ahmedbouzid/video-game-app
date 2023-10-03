4ce78c9edb08057f710defa86e22690f84af2ef3
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-10-03 19:55:07 +0200

    impliment logic for get games  by sort

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models/gameI';
+import { ServiceService } from 'src/app/services/service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
    sort !: string ;
 
-   constructor(){}
+   games !: Array <Game>
+   constructor(private service : ServiceService , private activatedRoute : ActivatedRoute){}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params : Params)=> {
+      if (params['game-search']) {
+        this.searchGames('metacrit' , params['game-search'])
+      } else {
+        this.searchGames('metacrit')
+      }
+    })
+
+  }
+  searchGames(sort : string , search ?: string) : void{
+    this.service
+    .getGameLList(sort , search)
+    .subscribe((gameList : APIResponse<Game>) => {
+      this.games = gameList.results ;
+      console.log('====================================');
+      console.log(gameList);
+      console.log('====================================');
+    })
+    console.log('====================================');
+    console.log(sort);
+    console.log('====================================');
+  }
+
+
 }
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -12,11 +12,11 @@ export class ServiceService {
   constructor(private http : HttpClient) { }
 
 
-  getGameLList(ordering : string , search : string) : Observable<APIResponse<Game>> {
+  getGameLList(ordering : string , search? : string) : Observable<APIResponse<Game>> {
     let params = new HttpParams().set('ordering' , ordering) ;
     if (search) {
       params = new HttpParams().set('ordering' , ordering).set('search' , search) ;
     }
-    return this.http.get<APIResponse<Game>>(`${environment.BASE_URL}/games`)
+    return this.http.get<APIResponse<Game>>(`${environment.BASE_URL}/games` , {params})
   }
 }
